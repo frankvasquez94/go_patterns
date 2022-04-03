7d69018f6b41e0a8b6a2c8551c8c02b03c9a492d
frankvasquez94 <fvasquezperez94@gmail.com>
2022-04-02 23:21:14 -0600

    optional design pattern

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 go patterns designs
 
 ## Description
-This repo contains patterns design implemented by using go language
+This repo contains Optionnal pattern design implemented by using go language.
+
+### Pattern description
+
+Option pattern is a functional programming pattern that is used to provide optional arguments to a function that can be used to modify its behavior.
+
+### Documentarion
+
+[Optional pattern doc]("https://medium.com/gitconnected/options-pattern-in-golang-9a0384a9d8db")
--- main.go
+++ main.go
@@ -1,10 +1,82 @@
 package main
 
-import "fmt"
+//
 
-const PatternName = "Pattern design"
+import (
+	"fmt"
+	"time"
+)
+
+const PatternName = "Option pattern design"
+
+// Main structure for a server
+// port is the unique required value
+// timeOut and maxConnection are optional
+type Server struct {
+	port           string
+	timeOut        time.Duration
+	maxConnections int
+}
+
+// Option Define a type Option that is
+// a function wich takes a Server pointer
+type Option func(*Server)
+
+func WithTimeOut(timeOut time.Duration) Option {
+	return func(s *Server) {
+		s.timeOut = timeOut
+	}
+}
+
+func WithMaxConnections(maxConnection int) Option {
+	return func(s *Server) {
+		s.maxConnections = maxConnection
+	}
+}
+
+func (s *Server) ToString() string {
+	resp := "Port: " + s.port
+
+	if s.timeOut != 0 {
+		resp += ", time out: " + fmt.Sprintf("%v", s.timeOut)
+	}
+
+	if s.maxConnections != 0 {
+		resp += ", max connections: " + fmt.Sprintf("%v", s.maxConnections)
+	}
+
+	return resp
+}
+
+func NewServer(port string, options ...Option) *Server {
+	server := &Server{
+		port: port,
+	}
+
+	for _, option := range options {
+		option(server)
+	}
+
+	return server
+}
 
 func main() {
 	p := fmt.Println
 	p(PatternName)
+
+	p("=========== Basic server =============")
+
+	basicServer := NewServer("90")
+	p(basicServer.ToString())
+
+	p("======== Server with optional timeout ===========")
+
+	withTimeOutServer := NewServer("90", WithTimeOut(3600))
+	p(withTimeOutServer.ToString())
+
+	p("============== Server with timeout and max connections")
+
+	withTimeOutAndMaxConnServer := NewServer("90", WithTimeOut(3600), WithMaxConnections(50))
+	p(withTimeOutAndMaxConnServer.ToString())
+
 }
